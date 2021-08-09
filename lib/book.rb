fddb8b7f354b0b8c5adacb2b5e4eaaf1e11e80bd
Jelena <jelena.durovic@powerhrg.com>
2021-08-09 09:13:23 -0400

    commit tree 031475f6d8f9fdff1ad8ae5e26714104779a7ec5

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book 
+    attr_reader :title
+
+    def initialize(title)
+      @title = title
+    end
+
+    attr_reader :author
+    attr_writer :author
+
+    attr_reader :page_count
+    attr_writer :page_count
+
+    attr_writer :genre
+    attr_reader :genre
+   
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+
+
+end 
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end 
+
+    attr_accessor :color
+
+    attr_reader :size
+    attr_writer :size
+
+    attr_reader :material
+    attr_writer :material
+
+    attr_reader :condition
+    attr_writer :condition
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+        
+
+    end 
+end 
